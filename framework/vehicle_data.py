b22be87e3d9179b097e96315358cdc38c6b56940
RakibCSE <rakibul.hossain365@gmail.com>
2023-08-18 03:49:41 +0600

    return type added for methods

--- framework/vehicle_data.py
+++ framework/vehicle_data.py
@@ -1,11 +1,13 @@
 import numpy as np
 
+from typing import Union
+
 
 class VehicleData:
     def __init__(self, filepath) -> None:
         self.data: np.ndarray = np.load(filepath)
 
-    def get_data_by_id(self, _id):
+    def get_data_by_id(self, _id) -> np.ndarray:
         """Returns filtered data by id
 
         Args:
@@ -17,7 +19,7 @@ class VehicleData:
         filtered_data: np.ndarray = self.data[self.data[:, 1] == _id]
         return filtered_data
 
-    def filter(self, filter_func=None):
+    def filter(self, filter_func=None) -> Union[np.ndarray, list]:
         """Filters the data segments by custom function
 
         Args:
