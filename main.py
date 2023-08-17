51c8c1238c9680f66131715194a74fcab969c4a8
RakibCSE <rakibul.hossain365@gmail.com>
2023-08-17 14:07:23 +0600

    get filtered segments and plot segments

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from framework.vehicle_data import VehicleData
+from framework.filters import length_filter
+from framework.plotter import Plotter
 
 
-if __name__ == '__main__':
-    data_path = './data.npy'
-    
+if __name__ == "__main__":
+    data_path = "./data.npy"
+
     # VehicleData instance
     obj = VehicleData(data_path)
-    
+
+    # Get data for specific Id
     id_data = obj.get_data_by_id(4)
-    
-    print(len(id_data))
 
     
+    filtered_segments = obj.filter(filter_func=length_filter)
+
+    # Create a Plotter instance
+    plotter = Plotter()
+
+    # Plot the filtered segments
+    plotter.plot(filtered_segments[:50])
